c20256ba5e3b4441a7d7be35b17f3457ef465bf7
ntninh18 <ntninh18@apcs.vn>
2019-11-08 22:26:43 +0700

    minor changes to Ex01

--- 18125106_W04_04/Ex01/NhanVien.cpp
+++ 18125106_W04_04/Ex01/NhanVien.cpp
@@ -24,7 +24,7 @@ NV::NV()
 {
 	staffID = nullptr;
 	fullname = nullptr;
-	dob = Date(1, 1, 1990);
+	dob = Date();
 	address = nullptr;
 }
 
@@ -32,7 +32,7 @@ NV::NV(char * id)
 {
 	staffID = id;
 	fullname = nullptr;
-	dob = Date(1, 1, 1990);
+	dob = Date();
 	address = nullptr;
 }
 
@@ -40,7 +40,7 @@ NV::NV(char * id, char * name)
 {
 	staffID = id;
 	fullname = name;
-	dob = Date(1, 1, 1990);
+	dob = Date();
 	address = nullptr;
 }
 
@@ -80,7 +80,7 @@ void NV::input()
 	cout << "Enter staff ID: ";
 	char *id = new char[100];
 	cin.ignore();
-	cin.getline(id, '\n');
+	cin.getline(id, 100);
 	delete[] staffID;
 	staffID = new char[strlen(id) + 1];
 	strcpy(staffID, id);
@@ -88,7 +88,7 @@ void NV::input()
 
 	cout << "Enter fullname: ";
 	char *name = new char[100];
-	cin.getline(name, '\n');
+	cin.getline(name, 100);
 	delete[] fullname;
 	fullname = new char[strlen(name) + 1];
 	strcpy(fullname, name);
@@ -100,7 +100,7 @@ void NV::input()
 	cout << "Enter address: ";
 	char *addr = new char[100];
 	cin.ignore();
-	cin.getline(addr, '\n');
+	cin.getline(addr, 100);
 	delete[] address;
 	address = new char[strlen(addr) + 1];
 	strcpy(address, addr);
